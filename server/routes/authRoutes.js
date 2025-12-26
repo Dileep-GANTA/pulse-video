f46c9b4e3a824df7e011e74f4e55a5c34bd37282
vyshnaviganta <vyshnaviganta100@gmaail.com>
2025-12-26 22:20:21 +0530

    Import authMiddleware  middleware

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -4,7 +4,7 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
 const jwt = require('jsonwebtoken');
-const auth = require('../middleware/auth'); // 
+const auth = require('../middleware/authMiddleware'); // 
 // ...
 
 // ... existing login/register routes ...
